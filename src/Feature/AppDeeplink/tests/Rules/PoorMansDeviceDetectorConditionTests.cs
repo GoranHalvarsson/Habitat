fffdc370d31bb8ecac2f4d3fad56c8259d7e9dc4
goran.halvarsson <goranhalvarsson@gmail.com>
2016-01-30 14:26:04 +0100

    Tests AppDeeplink

--- src/Feature/AppDeeplink/tests/Rules/PoorMansDeviceDetectorConditionTests.cs
+++ src/Feature/AppDeeplink/tests/Rules/PoorMansDeviceDetectorConditionTests.cs
@@ -38,7 +38,7 @@
 
     [Theory]
     [InlineAutoDbData("Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4", "iPhone", true)]
-    public void DoesUserAgentContainValueCondition(string userAgent, string containsUserAgentValue, bool expectedResult, Item contextItem, Db database)
+    public void DoesUserAgentContainValueCondition(string userAgent, string containsUserAgentValue, bool expectedResult, Db database)
     {
 
 
