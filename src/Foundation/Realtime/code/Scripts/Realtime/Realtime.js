aa27c1b85db054691a595c85f1fe3117d8ead5c3
GoranHalvarsson <goranhalvarsson@gmail.com>
2016-07-23 16:40:57 +0200

    Small fix

--- src/Foundation/Realtime/code/Scripts/Realtime/Realtime.js
+++ src/Foundation/Realtime/code/Scripts/Realtime/Realtime.js
@@ -16,12 +16,6 @@ Habitat.Realtime = {
 
         var connection = jQuery.hubConnection();
 
-        //connection.hub.disconnected(function () {
-        //    setTimeout(function () {
-        //        $.connection.hub.start();
-        //    }, 5000); // Restart connection after 5 seconds.
-        //});
-
         var realTimeConnector = connection.createHubProxy("RealtimeConnector");
 
         realTimeConnector.on("onSetClientData", function (text) {
@@ -33,16 +27,6 @@ Habitat.Realtime = {
             var jsonObject = Habitat.Realtime.GetUserMetaData();
 
             realTimeConnector.invoke("SendClientMetaData", jsonObject).done();
-            //.done(function (result) {
-
-            //    if (!result) {
-            //        window.RealTimeConnector.invoke("SendClientBaseData", jsonObject).done();
-            //    }
-
-            //    window.RealTimeConnector.invoke("SendClientMetaData", jsonObject).done();
-
-
-            //});
         });
 
         realTimeConnector.on("onWhereIs", function () {
@@ -52,7 +36,7 @@ Habitat.Realtime = {
                     return;
 
                 var locationUserObject = {};
-                locationUserObject["Coordinates"] = Habitat.ClientTracker.StringFormat("{0},{1}", position.coords.latitude, position.coords.longitude, "1");
+                locationUserObject["Coordinates"] = Habitat.ClientTracker.StringFormat("{0},{1}", position.coords.latitude, position.coords.longitude);
 
                 var jsonObject = {};
                 jsonObject["Container"] = locationUserObject;
@@ -64,31 +48,15 @@ Habitat.Realtime = {
 
         connection.start().done();
 
-        //connection.start().done(function () {
-        //    window.RealTimeConnector.invoke("subscribeToVisitors");
-        //    window.RealTimeConnector.invoke("getClientDataFromVisitors");
-        //});
-
-
-
-        //How to continuously reconnect
-        //In some applications you might want to automatically re-establish a connection after it has been lost and the 
-        //attempt to reconnect has timed out. To do that, you can call the Start method from your Closed event handler 
-        //(disconnected event handler on JavaScript clients). You might want to wait a period of time before calling Start in order to 
-        //avoid doing this too frequently when the server or the physical connection are unavailable. The following code sample is for 
-        //a JavaScript client using the generated proxy.
-
-
-
     },
     GetUserMetaData: function () {
 
         var baseUserObject = {};
-        baseUserObject["Language"] = jQuery("body").data("currentlanguage");
-        baseUserObject["SiteName"] = jQuery("body").data("currentsite");
+        baseUserObject["Language"] = document.documentElement.lang;
+        baseUserObject["SiteName"] = window.location.host;
         baseUserObject["IpAddress"] = jQuery("body").data("currentipaddress");
         baseUserObject["PageUrl"] = window.location.pathname;
-        //baseUserObject["LastUpdate"] = new Date().toJSON().slice(0, 10);
+
         var jsonObject = {};
         jsonObject["Container"] = baseUserObject;
 
---
+++ src/Foundation/SitecoreCustomizations/code/Extensions/AnalyticsExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace VisionsInCode.Foundation.SitecoreCustomizations.Extensions
+{
+	using System.Net;
+
+	public static class AnalyticsExtensions
+	{
+		public static string GetFriendlyIpAddress(this byte[] bytes)
+		{
+			IPAddress address = new IPAddress(bytes);
+
+			return address.ToString();
+		}
+
+	}
+}
